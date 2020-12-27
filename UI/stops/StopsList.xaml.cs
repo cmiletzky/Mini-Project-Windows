50b534c8c1e79d5644827959f37209363ec67f12
יעקב מרגלית <ymargali@g.jct.ac.il>
2020-12-27 12:19:43 +0200

    erf

---
+++ UI/stops/StopsList.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL.stops
+{
+    /// <summary>
+    /// Interaction logic for StopsList.xaml
+    /// </summary>
+    public partial class StopsList : Window
+    {
+        public StopsList(IEnumerable<BO.Station> list)
+        {
+            InitializeComponent();
+            stop_list.ItemsSource = list;
+            stop_list.DisplayMemberPath = "Code";
+        }
+    }
+}
