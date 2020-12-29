db3ec4c27b8c22f42592671894579e8f07b5bf67
יעקב מרגלית <ymargali@g.jct.ac.il>
2020-12-29 14:52:35 +0200

    d

--- UI/stops/StopsList.xaml.cs
+++ UI/stops/StopsList.xaml.cs
@@ -22,6 +22,7 @@ namespace PL.stops
     {
          BO.LineBus  line;
         BO.Station preStop;
+        BO.Station firstStop;
         ListBox listStopOfLine;
         public StopsList(IEnumerable<BO.Station> list,ref BO.LineBus lineTo, ref ListBox listStopOfLine, BO.Station preStop)
         {
@@ -47,21 +48,26 @@ namespace PL.stops
 
         private void add_stop_to_line_Click(object sender, RoutedEventArgs e)
         {
-            BO.Station a = (BO.Station)stop_list.SelectedValue;
+            BO.Station newStop = (BO.Station)stop_list.SelectedValue;
             try
             {
+
+
                 //TODO לבדוק תחנות עוקובת 
-                if (MainWindow.bl.CheckAdjacentStatision(preStop.Code, a.Code) == false)
+                if ( preStop != null&&MainWindow.bl.CheckAdjacentStatision(preStop.Code, newStop.Code) == false)
                 {
-                    new GetDataStops(preStop, a).ShowDialog();
+                    new GetDataStops(preStop, newStop).ShowDialog();
+                    MainWindow.bl.AddStopLine(newStop.Code, line.LineNum, preStop.IndexInLine);
+                    Close();
                 }
-                if (preStop == null)
+               else if (preStop == null && MainWindow.bl.CheckAdjacentStatision(preStop.Code, newStop.Code) == false)
                 {
-                    MainWindow.bl.AddStopLine(a.Code, line.LineNum, 0);
+                    new GetDataStopsForFirst(firstStop, newStop).ShowDialog();
+                    MainWindow.bl.AddStopLine(newStop.Code, line.LineNum, 0);
                 }
                 else
                 {
-                    MainWindow.bl.AddStopLine(a.Code, line.LineNum, preStop.IndexInLine);
+                    MainWindow.bl.AddStopLine(newStop.Code, line.LineNum, preStop.IndexInLine);
                 }
 
                 Close();
